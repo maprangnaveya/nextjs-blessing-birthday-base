ef857456eb5c02e119a3fd988b1d9294e3cb05a2
Naveya Jintaya <naveya@divertise.asis>
2024-03-12 21:24:34 +0700

    Init nextjs project.

---
+++ app/blessings/(overview)/page.tsx
@@ -0,0 +1,16 @@
+import { lusitana } from '@/app/ui/fonts';
+
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Blessings
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+       <h2>View blessings</h2>
+      </div>
+
+    </main>
+  );
+}
---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,2 @@
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data

e71d99b546579ce1197f847336d3c8f59b41ad96
Naveya Jintaya <naveya@divertise.asis>
2024-03-21 00:15:32 +0700

    Set view blessings page to revalidate every 10 mins.

--- app/blessings/(overview)/page.tsx
+++ app/blessings/(overview)/page.tsx
@@ -1,6 +1,7 @@
 import BlessingList from '@/app/ui/blessings/blessing-list';
 import { lusitana } from '@/app/ui/fonts';
 
+export const revalidate = 600 // revalidate at most every 10 mins
 
 export default async function Page() {
 

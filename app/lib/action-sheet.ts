a2f53d68db067e7943a8c713a290042f96e126ae
Naveya Jintaya <naveya@divertise.asis>
2024-03-20 19:40:02 +0700

    Skip sheet headers row.

--- app/lib/action-sheet.ts
+++ app/lib/action-sheet.ts
@@ -1,4 +1,4 @@
-import {google} from "googleapis";
+import { google } from "googleapis";
 
 export const getAllBlessings = async () => {
     const auth = new google.auth.GoogleAuth({
@@ -6,18 +6,18 @@ export const getAllBlessings = async () => {
             client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
             private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n")
         },
-        scopes:["https://www.googleapis.com/auth/spreadsheets"]
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"]
     })
 
-    const sheets = google.sheets({version: "v4", auth: auth})
-    const range = "Sheet1!A:C"
+    const sheets = google.sheets({ version: "v4", auth: auth })
+    const range = "Sheet1!A2:C"
     try {
         const response = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.GOOGLE_SHEET_ID,
             range: range
         })
         return response.data.values
-    } catch(error){
+    } catch (error) {
         console.log("!!! get sheets data failed with ", error);
         return []
     }

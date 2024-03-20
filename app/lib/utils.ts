691d57b365268bcd73189df842b39558c9f0fe20
Naveya Jintaya <naveya@divertise.asis>
2024-03-20 18:00:56 +0700

    Create utils function to get date text.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,39 +1,13 @@
-import { Revenue } from './definitions';
 
-export const formatCurrency = (amount: number) => {
-  return (amount / 100).toLocaleString('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  });
-};
+import dayjs from 'dayjs';
 
-export const formatDateToLocal = (
-  dateStr: string,
-  locale: string = 'en-US',
-) => {
-  const date = new Date(dateStr);
-  const options: Intl.DateTimeFormatOptions = {
-    day: 'numeric',
-    month: 'short',
-    year: 'numeric',
-  };
-  const formatter = new Intl.DateTimeFormat(locale, options);
-  return formatter.format(date);
-};
+export const getDateText = (dateStr: string): string => {
+    const targetDate = dayjs(dateStr);
 
-export const generateYAxis = (revenue: Revenue[]) => {
-  // Calculate what labels we need to display on the y-axis
-  // based on highest record and in 1000s
-  const yAxisLabels = [];
-  const highestRecord = Math.max(...revenue.map((month) => month.revenue));
-  const topLabel = Math.ceil(highestRecord / 1000) * 1000;
+      return targetDate.format('DD MMM YYYY').toUpperCase();
+}
 
-  for (let i = topLabel; i >= 0; i -= 1000) {
-    yAxisLabels.push(`$${i / 1000}K`);
-  }
 
-  return { yAxisLabels, topLabel };
-};
 
 export const generatePagination = (currentPage: number, totalPages: number) => {
   // If the total number of pages is 7 or less,

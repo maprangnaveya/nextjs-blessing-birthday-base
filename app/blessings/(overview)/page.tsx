eadbbdfd51f64e02d5b5e96ccd43ad84a9147fff
Naveya Jintaya <naveya@divertise.asis>
2024-06-23 15:41:24 +0700

    feat: comment unused component

--- app/blessings/(overview)/page.tsx
+++ app/blessings/(overview)/page.tsx
@@ -1,6 +1,6 @@
 import { getAllBlessings } from '@/app/lib/action-sheet';
-import BlessingList from '@/app/ui/blessings/blessing-list';
-import BlessingGridList from '@/app/ui/blessings/blessing-grid-slide';
+// import BlessingList from '@/app/ui/blessings/blessing-list';
+// import BlessingGridList from '@/app/ui/blessings/blessing-grid-slide';
 import BlessingPagination from '@/app/ui/blessings/blessing-pagination';
 
 export const revalidate = 600 // revalidate at most every 10 mins
